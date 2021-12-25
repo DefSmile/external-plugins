e5cd6df30ef8af33236665d5c5961ce9b3f27bdb
smile <notsmile@protonmail.com>
2021-12-25 11:32:56 -0500

    Initial Commit

--- herblore_aio/src/java/com/shibe/herblore/HerblorePlugin.java
+++ herblore_aio/src/java/com/shibe/herblore/HerblorePlugin.java
@@ -6,6 +6,7 @@ import net.runelite.api.events.GameTick;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDependency;
 import net.runelite.client.plugins.PluginDescriptor;
 import org.pf4j.Extension;
 
--- herblore_aio/src/java/com/shibe/herblore/HerbloreState.java
+++ herblore_aio/src/java/com/shibe/herblore/HerbloreState.java
@@ -1,4 +1,15 @@
 package java.com.shibe.herblore;
 
-public class HerbloreState {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum HerbloreState {
+    WALKKING("Walking"),
+    MAKING_POTION("Making Potion"),
+    DOING_QUEST("Doing Quest");
+
+    @Getter
+    private final String name;
+
 }
